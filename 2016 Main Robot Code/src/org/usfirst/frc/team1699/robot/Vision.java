7cfe3f067369f46822f1aba8db31b9a6f638905f
Jakob Misbach <squirlemaster@gmail.com>
2016-02-12 08:14:34 -0500

    Finished v1 vision code

--- 2016 Main Robot Code/src/org/usfirst/frc/team1699/robot/Vision.java
+++ 2016 Main Robot Code/src/org/usfirst/frc/team1699/robot/Vision.java
@@ -1,12 +1,34 @@
 package org.usfirst.frc.team1699.robot;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 public class Vision {
 	
-	BufferedImage image;
+	private BufferedImage image;
+	private Boolean[][] imgArr;
 	
 	public Vision(BufferedImage image){
 		this.image = image;
+	}
+	
+	public Boolean[][] lookColor(int red, int green, int blue){
+		imgArr = new Boolean[image.getHeight()][image.getWidth()];
+		
+		 for (int y = 0; y < image.getHeight(); y++) {
+             for (int x = 0; x < image.getWidth(); x++) {
+            	             	 
+            	 int c = image.getRGB(x, y);
+            	 Color color = new Color(c);
+            	 
+            	 if (color.getRed() == red && color.getGreen() == green && color.getBlue() == blue){
+            		 imgArr[y][x] = true;
+            	 }else{
+            		 imgArr[y][x] = false;
+            	 }
+             }
+		 }
+		 
+		 return imgArr;
 	}
 }
