d7532b81bb6dec949412455f458547a918e3e659
squirlemaster42 <squirlemaster@gmail.com>
2016-02-12 15:26:57 -0500

    Added comments to explain code

--- 2016 Main Robot Code/src/org/usfirst/frc/team1699/robot/Vision.java
+++ 2016 Main Robot Code/src/org/usfirst/frc/team1699/robot/Vision.java
@@ -6,7 +6,10 @@ import java.util.Arrays;
 
 public class Vision {
 
+	//Image variable
 	private BufferedImage image;
+	
+	//Array for image
 	private Boolean[][] imgArr;
 	
 	public Vision(BufferedImage image){
@@ -14,17 +17,23 @@ public class Vision {
 	}
 	
 	public Boolean[][] lookColor(int red, int green, int blue){
+		//Sets array size
 		imgArr = new Boolean[image.getHeight()][image.getWidth()];
 		
+		//Runs through image pixel by pixel
 		 for (int y = 0; y < image.getHeight(); y++) {
              for (int x = 0; x < image.getWidth(); x++) {
             	             	 
+            	 //Variables to help find colors
             	 int c = image.getRGB(x, y);
             	 Color color = new Color(c);
             	 
+            	 //Looks for a pixel of a certain color in the image
             	 if (color.getRed() == red && color.getGreen() == green && color.getBlue() == blue){
+            		 //If the color is found that spot in the array is set to true
             		 imgArr[y][x] = true;
             	 }else{
+            		//If the color is not found that spot in the array is set to false
             		 imgArr[y][x] = false;
             	 }
              }
@@ -33,6 +42,8 @@ public class Vision {
 		 return imgArr;
 	}
 	
+	
+	//Getters and setters
 	public BufferedImage getImage() {
 		return image;
 	}
