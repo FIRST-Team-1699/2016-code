4cf9bcacb98688abe3f3299c614994bf039e9ec7
Jakob Misbach <squirlemaster@gmail.com>
2016-02-12 07:45:25 -0500

    Added starting vision code

---
+++ 2016 Main Robot Code/src/org/usfirst/frc/team1699/robot/Vision.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1699.robot;
+
+import java.awt.image.BufferedImage;
+
+public class Vision {
+	
+	BufferedImage image;
+	
+	public Vision(BufferedImage image){
+		this.image = image;
+	}
+}
