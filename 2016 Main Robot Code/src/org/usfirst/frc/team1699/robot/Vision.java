10f24537166c1e71f7710840ad045a5902f492ae
Connor <thatging3rkid@users.noreply.github.com>
2017-03-09 15:58:59 -0500

    cleaning up the code

--- 2016 Main Robot Code/src/org/usfirst/frc/team1699/robot/Vision.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team1699.robot;
-
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
-
-public class Vision {
-
-	NetworkTable table;
-	
-}
---
+++ src/org/usfirst/frc/team1699/robot/Vision.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team1699.robot;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+public class Vision {
+
+	NetworkTable table;
+	
+}
